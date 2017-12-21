86f13e007881fa48d76e0c87b6966655c9a8fd57
Keegan <jonesk74@uw.edu>
2017-12-21 11:12:53 -0800

    Use filesystem library to make paths and compiler more robust.

--- include/Compiler.hpp
+++ include/Compiler.hpp
@@ -3,9 +3,10 @@
 #define SHADER_TOOLS_COMPILER_HPP
 #include <vulkan/vulkan.h>
 #include <string>
-#include <unordered_map>
+#include <map>
 #include <vector>
 #include <cstdint>
+#include <filesystem>
 
 namespace st {
 
@@ -15,12 +16,13 @@ namespace st {
         ~ShaderCompiler() = default;
         
         const std::vector<uint32_t>& Compile(const std::string& path_to_source, const VkShaderStageFlags& stage);
-        bool HasShader(const std::string& binary_path);
+        bool HasShader(const std::string& binary_path) const;
         const std::vector<uint32_t>& GetBinary(const std::string& binary_path) const;
         void AddBinary(const std::string& path, std::vector<uint32_t> binary_data);
         
+        
     private:
-        std::unordered_map<std::string, std::vector<uint32_t>> compiledShaders;
+        std::map<std::experimental::filesystem::path, std::vector<uint32_t>> compiledShaders;
     };
 
 }
--- src/Compiler.cpp
+++ src/Compiler.cpp
@@ -3,12 +3,21 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+
+namespace fs = std::experimental::filesystem;
+
 namespace st {
 
-    const std::vector<uint32_t>& ShaderCompiler::Compile(const std::string& path_to_source, const VkShaderStageFlags& stage) {
-
+    const std::vector<uint32_t>& ShaderCompiler::Compile(const std::string& path_to_source_str, const VkShaderStageFlags& stage) {
+        fs::path path_to_source(path_to_source_str);
         if(compiledShaders.count(path_to_source) != 0) {
             return compiledShaders.at(path_to_source);
+        }
+
+        // First check to verify the path given exists.
+        if (!fs::exists(path_to_source)) {
+            std::cerr << "Given shader source path/file does not exist!\n";
+            throw std::runtime_error("Failed to open/find given shader file.");
         }
         
         shaderc::Compiler compiler;
@@ -46,22 +55,13 @@ namespace st {
                 throw std::domain_error("Invalid shader stage bitfield, or shader stage not supported on current platform!");
         }
 
-        const size_t idx = path_to_source.find_last_of('/');
-        std::string filename;
-        if (idx != std::string::npos) {
-            filename = std::string(path_to_source.cbegin() + idx, path_to_source.cend());
-        }
-        else {
-            filename = path_to_source;
-        }
-
         std::ifstream input_file(path_to_source);
         if(!input_file.is_open()) {
             throw std::runtime_error("Failed to open supplied file in GLSLCompiler!");
         }
         const std::string source_code((std::istreambuf_iterator<char>(input_file)), (std::istreambuf_iterator<char>()));
-
-        shaderc::SpvCompilationResult result = compiler.CompileGlslToSpv(source_code, shader_stage, filename.c_str());
+        const std::string file_name = path_to_source.filename().string();
+        shaderc::SpvCompilationResult result = compiler.CompileGlslToSpv(source_code, shader_stage, file_name.c_str());
         if(result.GetCompilationStatus() != shaderc_compilation_status_success) {
             const std::string err_msg = result.GetErrorMessage();
             std::cerr << "Shader compiliation failed: " << err_msg.c_str() << "\n";
@@ -69,28 +69,29 @@ namespace st {
             throw std::runtime_error(except_msg.c_str());
         }
 
-        auto inserted = compiledShaders.insert(std::make_pair(path_to_source, std::vector<uint32_t>{ result.begin(), result.end() }));
+        auto inserted = compiledShaders.insert(std::make_pair(fs::absolute(path_to_source), std::vector<uint32_t>{ result.begin(), result.end() }));
         if(!inserted.second) {
             throw std::runtime_error("Failed to insert shader into compiled shader map!");
         }
         return (inserted.first)->second;   
     }
 
-    bool ShaderCompiler::HasShader(const std::string & binary_path) {
-        return compiledShaders.count(binary_path) != 0;
+    bool ShaderCompiler::HasShader(const std::string & binary_path) const {
+        const fs::path absolute_path = fs::absolute(fs::path(binary_path));
+        return compiledShaders.count(absolute_path) != 0;
     }
 
     const std::vector<uint32_t>& ShaderCompiler::GetBinary(const std::string & binary_path) const {
-        if (compiledShaders.count(binary_path) != 0) {
-            return compiledShaders.at(binary_path);
+        if (!HasShader(binary_path)) {
+            throw std::runtime_error("Requested binary not found!");
         }
-        else {
-            throw std::runtime_error("Requested shader does not exist!");
-        }
+        const fs::path absolute_path = fs::absolute(fs::path(binary_path));
+        return compiledShaders.at(absolute_path);
     }
 
     void ShaderCompiler::AddBinary(const std::string & path, std::vector<uint32_t> binary_data) {
-        auto inserted = compiledShaders.insert(std::make_pair(path, std::move(binary_data)));
+        const fs::path absolute_path = fs::absolute(fs::path(path));
+        auto inserted = compiledShaders.insert(std::make_pair(absolute_path, std::move(binary_data)));
         if (!inserted.second) {
             throw std::runtime_error("Tried to insert already-stored shader binary!");
         }
