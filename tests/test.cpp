f36bdab3b374f3b7a0440597781cd9269c1b82d3
Keegan <jonesk74@uw.edu>
2018-04-05 13:40:56 -0700

    Update tests, update third-party libraries

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,15 +1,17 @@
 #include <map>
 #include <iostream>
 #include <vector>
+#include <array>
+#include <fstream>
 #include "Compiler.hpp"
 #include "BindingGenerator.hpp"
+#include "ShaderGenerator.hpp"
 #include <experimental/filesystem>
 
 namespace fs = std::experimental::filesystem;
 
-int main(int argc, char* argv[]) {
-    const std::vector<std::string> args(argv + 1, argv + argc);
-    std::vector<std::vector<uint32_t>> binaries;
+void CompiliationTest() {
+    const std::vector<std::string> args{ "../tests/Clustered.vert", "../tests/Clustered.frag" };
     using namespace st;
     ShaderCompiler compiler;
     BindingGenerator parser;
@@ -21,4 +23,42 @@ int main(int argc, char* argv[]) {
         parser.ParseBinary(handle);        
     }
     parser.SaveToJSON("out.json");
+}
+
+void GenerationTest() {
+    const std::string fname{ "MergeSort" };
+    std::string ComputeShaderTest{ "../fragments/volumetric_forward/compute/" };
+    ComputeShaderTest += fname + ".comp";
+    using namespace st;
+    ShaderGenerator gen(VK_SHADER_STAGE_COMPUTE_BIT);
+    std::array<const char*, 1> IncludePaths{ "../fragments/volumetric_forward" };
+    gen.AddResources("../fragments/volumetric_forward/Resources.glsl");
+    gen.AddBody(ComputeShaderTest.c_str(), 1, IncludePaths.data());
+    size_t len = 0;
+    gen.GetFullSource(&len, nullptr);
+    std::vector<char> generated_src(len);
+    gen.GetFullSource(&len, generated_src.data());
+    std::string src_string{ generated_src.cbegin(), generated_src.cend() };
+    std::ofstream output_src(fname + "_Generated.glsl");
+    if (!output_src.is_open()) {
+        throw std::runtime_error("Couldn't open output for generated glsl.");
+    }
+    output_src << src_string;
+    output_src.flush();
+    output_src.close();
+
+    ShaderCompiler compiler;
+    Shader handle = compiler.Compile(fname.c_str(), src_string.c_str(), src_string.size(), VK_SHADER_STAGE_COMPUTE_BIT);
+    std::string compiled_fname{ fname + "_Compiled.glsl" };
+    compiler.SaveBinaryBackToText(handle, compiled_fname.c_str());
+
+    BindingGenerator parser;
+    parser.ParseBinary(handle);
+    parser.SaveToJSON(std::string(fname + ".json").c_str());
+
+}
+
+int main(int argc, char* argv[]) {
+    //CompiliationTest();
+    GenerationTest(); 
 }
