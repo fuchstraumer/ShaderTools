ac1ccd8e166ab79296a3bcfe3f1c17085ba2f6f3
Keegan <jonesk74@uw.edu>
2018-04-22 10:28:02 -0700

    Add functions removed from impl

--- src/parser/BindingGenerator.cpp
+++ src/parser/BindingGenerator.cpp
@@ -15,4 +15,20 @@ namespace st {
         return *this;
     }
 
+    uint32_t BindingGenerator::GetNumSets() const noexcept {
+        return static_cast<uint32_t>(impl->sortedSets.size());
+    }    
+    
+    void BindingGenerator::Clear() {
+        impl.reset();
+        impl = std::make_unique<BindingGeneratorImpl>();
+    }
+
+    BindingGeneratorImpl * BindingGenerator::GetImpl() {
+        return impl.get();
+    }
+
+    void BindingGenerator::ParseBinary(const Shader & shader) {
+        impl->parseBinary(shader);
+    }
 }
