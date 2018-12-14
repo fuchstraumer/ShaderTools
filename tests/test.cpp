345b7ee08633f13df944a33a4db8088e77304d7c
Keegan <jonesk74@uw.edu>
2018-12-14 10:08:42 -0800

    update tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "core/Shader.hpp"
 #include "core/ShaderPack.hpp"
+#include "../src/util/ShaderPackBinary.hpp"
 #include <array>
 #include "easylogging++.h"
 INITIALIZE_EASYLOGGINGPP
@@ -38,51 +39,13 @@ int main(int argc, char* argv[]) {
     callbacks.GetFOVY = &fov_y;
     
     ShaderPack pack("../fragments/volumetric_forward/ShaderPack.lua");
-    std::vector<std::string> group_names;
-    {
-        auto names = pack.GetShaderGroupNames();
-        for (size_t i = 0; i < names.NumStrings; ++i) {
-            group_names.emplace_back(names.Strings[i]);
-        }
 
-    }
+    ShaderPackBinary* binarization_of_pack = new ShaderPackBinary();
 
-    using bindings_map_t = std::map<uint32_t, std::vector<VkDescriptorSetLayoutBinding>>;
-    using pack_bindings_map_t = std::unordered_map<std::string, bindings_map_t>;
-    using pack_resource_per_group_names_t = std::unordered_map<std::string, std::vector<std::string>>;
-    using pack_specialization_constants_map_t = std::unordered_map<std::string, std::vector<SpecializationConstant>>;
-    pack_specialization_constants_map_t constants_map;
-    pack_bindings_map_t all_bindings;
+    CreateShaderPackBinary(&pack, binarization_of_pack);
 
-    for (const auto& group_name : group_names) {
-        const Shader* group = pack.GetShaderGroup(group_name.c_str());
-        size_t num_sets = group->GetNumSetsRequired();
-        bindings_map_t group_bindings;
-        for (size_t i = 0; i < num_sets; ++i) {
-            size_t num_bindings = 0;
-            group->GetSetLayoutBindings(i, &num_bindings, nullptr);
-            std::vector<VkDescriptorSetLayoutBinding> bindings(num_bindings);
-            group->GetSetLayoutBindings(i, &num_bindings, bindings.data());
-            group_bindings.emplace(static_cast<uint32_t>(i), bindings);
-        }
+    DestroyShaderPackBinary(binarization_of_pack);
+    delete binarization_of_pack;
 
-        std::vector<std::string> block_names;
-        {
-            auto retrieved_block_names = group->GetUsedResourceBlocks();
-            for (size_t i = 0; i < retrieved_block_names.NumStrings; ++i) {
-                block_names.emplace_back(retrieved_block_names.Strings[i]);
-            }
-        }
-        size_t num_spcs = 0;
-        group->GetSpecializationConstants(&num_spcs, nullptr);
-        std::vector<SpecializationConstant> constants(num_spcs);
-        group->GetSpecializationConstants(&num_spcs, constants.data());
-        if (!constants.empty()) {
-            constants_map.emplace(group_name, constants);
-        }
-
-        all_bindings.emplace(group_name, group_bindings);
-    }
-
-    std::cerr << "Tests complete.";
+    std::cerr << "Tests complete.\n";
 }
