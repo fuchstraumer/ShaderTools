b189d5e5fac1b90a3eb493257b905ad5fcdbb0f0
Keegan <jonesk74@uw.edu>
2018-12-14 09:19:50 -0800

    First pass at binarization of a single shader

--- src/util/ShaderPackBinary.cpp
+++ src/util/ShaderPackBinary.cpp
@@ -2,8 +2,11 @@
 #include "ShaderFileTracker.hpp"
 #include "core/Shader.hpp"
 #include <vector>
+#include <string>
 
 namespace st {
+
+    namespace fs = std::experimental::filesystem;
 
     void CreateShaderBinary(const Shader* src, ShaderBinary* binary_dst) {
 
@@ -26,7 +29,64 @@ namespace st {
             binary_dst->StageIDs[i] = stages[i].ID;
         }
 
-        // now get last write times of files
+        // now get data we need from ftracker
+        auto& ftracker = ShaderFileTracker::GetFileTracker();
+
+        uint32_t total_req_path_length{ 0 };
+        uint32_t total_req_src_length{ 0 };
+        uint32_t total_req_binary_length{ 0 };
+
+        for (size_t i = 0; i < num_stages; ++i) {
+            // write time first
+            // TODO: validate this converts right. need to do UTC too. 
+            const fs::path& stage_path = ftracker.BodyPaths.at(stages[i]);
+            auto file_write_time = fs::file_time_type::clock::to_time_t(fs::last_write_time(stage_path));
+            binary_dst->LastWriteTimes[i] = uint64_t(file_write_time);
+
+            // get lengths of paths and source strings, so we can allocate memory, then write strings
+            binary_dst->PathLengths[i] = stage_path.string().length();
+            binary_dst->SrcStringLengths[i] = ftracker.FullSourceStrings.at(stages[i]).length();
+            binary_dst->BinaryLengths[i] = ftracker.Binaries.at(stages[i]).size();
+
+            total_req_path_length += binary_dst->PathLengths[i] + 1;
+            total_req_src_length += binary_dst->SrcStringLengths[i] + 1;
+            total_req_binary_length += binary_dst->BinaryLengths[i];
+        }
+
+        binary_dst->Paths = new char[total_req_path_length];
+        binary_dst->SourceStrings = new char[total_req_src_length];
+        binary_dst->Binaries = new uint32_t[total_req_binary_length];
+
+        uint32_t current_path_offset{ 0 };
+        uint32_t current_src_offset{ 0 };
+        uint32_t current_binary_offset{ 0 };
+
+        for (size_t i = 0; i < num_stages; ++i) {
+
+            const std::string path_str = ftracker.BodyPaths.at(stages[i]).string();
+            std::memcpy(binary_dst->Paths + current_path_offset, path_str.data(), path_str.length());
+            current_path_offset += path_str.length();
+
+            const std::string& full_src_str = ftracker.FullSourceStrings.at(stages[i]);
+            std::memcpy(binary_dst->SourceStrings + current_src_offset, full_src_str.data(), full_src_str.length());
+            current_src_offset += full_src_str.length();
+
+            const auto& stage_binary = ftracker.Binaries.at(stages[i]);
+            std::memcpy(binary_dst->Binaries + current_binary_offset, stage_binary.data(), stage_binary.size());
+            current_binary_offset += stage_binary.size();
+
+        }
+
+        binary_dst->TotalLength = sizeof(uint32_t) + sizeof(uint64_t) + sizeof(uint32_t); // magic + length field + num_stages field
+        binary_dst->TotalLength += sizeof(uint64_t) * num_stages; // stage handles
+        binary_dst->TotalLength += sizeof(uint64_t) * num_stages; // last write times
+        binary_dst->TotalLength += sizeof(uint32_t) * num_stages; // path length count
+        binary_dst->TotalLength += total_req_path_length * sizeof(char); // path strings
+        binary_dst->TotalLength += sizeof(uint32_t) * num_stages; // source string count
+        binary_dst->TotalLength += total_req_src_length * sizeof(char); // source strings
+        binary_dst->TotalLength += sizeof(uint32_t) * num_stages; // binary count
+        binary_dst->TotalLength += total_req_binary_length * sizeof(uint32_t); // binaries
+
     }
 
     void DestroyShaderBinary(ShaderBinary * binary) {
