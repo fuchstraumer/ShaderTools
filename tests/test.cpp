b406ab2c4583a1dbe6337862c2ff9a1b7239c8c0
Keegan <jonesk74@uw.edu>
2019-09-08 14:32:13 -0700

    Update CMakeLists, update submodules, and clean up tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,42 +8,14 @@
 #include "easylogging++.h"
 INITIALIZE_EASYLOGGINGPP
 
-static int screen_x() {
-    return 1920;
-}
-
-static int screen_y() {
-    return 1080;
-}
-
-static double z_near() {
-    return 0.1;
-}
-
-static double z_far() {
-    return 3000.0;
-}
-
-static double fov_y() {
-    return 75.0;
-}
-
 void VerifyLoadedPack(st::ShaderPackBinary* bin0, st::ShaderPackBinary* bin1) {
 
 }
 
 int main(int argc, char* argv[]) {
     using namespace st; 
-
-    auto& callbacks = ShaderPack::RetrievalCallbacks();
     
-    callbacks.GetScreenSizeX = &screen_x;
-    callbacks.GetScreenSizeY = &screen_y;
-    callbacks.GetZNear = &z_near;
-    callbacks.GetZFar = &z_far;
-    callbacks.GetFOVY = &fov_y;
-    
-    for (size_t i = 0; i < 100; ++i) {
+    for (size_t i = 0; i < 4; ++i) {
 
         std::chrono::high_resolution_clock::time_point beforeExec;
         beforeExec = std::chrono::high_resolution_clock::now();
