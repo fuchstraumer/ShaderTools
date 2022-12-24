4caae29804e7be3d24b2d65bfc7bca4b484c8df9
fuchstraumer <fuchstraumer@gmail.com>
2022-12-23 19:19:35 -0800

    Get the basic test running
    Content still works, somehow! Kinda impressed...

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,41 +2,28 @@
 #include <vector>
 #include "core/Shader.hpp"
 #include "core/ShaderPack.hpp"
-#include "../src/util/ShaderPackBinary.hpp"
 #include "../src/util/ShaderFileTracker.hpp"
 #include <array>
-#include "easylogging++.h"
-INITIALIZE_EASYLOGGINGPP
+#include <iostream>
 
-void VerifyLoadedPack(st::ShaderPackBinary* bin0, st::ShaderPackBinary* bin1) {
-
-}
-
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     using namespace st; 
     
-    for (size_t i = 0; i < 1; ++i) {
+    for (size_t i = 0; i < 100; ++i)
+    {
 
         std::chrono::high_resolution_clock::time_point beforeExec;
         beforeExec = std::chrono::high_resolution_clock::now();
         ShaderPack pack("../fragments/volumetric_forward/volumetric_forward.yaml");
         std::chrono::duration<double, std::milli> work_time = std::chrono::high_resolution_clock::now() - beforeExec;
-        LOG(INFO) << "Conventional creation of ShaderPack took: " << work_time.count() << "ms";
-        ShaderPackBinary* binarization_of_pack = CreateShaderPackBinary(&pack);
-        SaveBinaryToFile(binarization_of_pack, "VolumetricForwardPack.stbin");
+        std::cout << "Conventional creation of ShaderPack took: " << work_time.count() << "ms\n";
 
+        //DumpProgramStateToCache();
         ClearProgramState();
 
-        ShaderPackBinary* reloaded_pack = LoadShaderPackBinary("VolumetricForwardPack.stbin");
-        beforeExec = std::chrono::high_resolution_clock::now();
-        ShaderPack binary_loaded_pack(reloaded_pack);
-        work_time = std::chrono::high_resolution_clock::now() - beforeExec;
-        LOG(INFO) << "Creation of ShaderPack using saved binary took: " << work_time.count() << "ms";
-
-        DestroyShaderPackBinary(binarization_of_pack);
-        DestroyShaderPackBinary(reloaded_pack);
     }
 
-    std::cerr << "Tests complete.\n";
+    std::cout << "Tests complete.\n";
     return 0;
 }
