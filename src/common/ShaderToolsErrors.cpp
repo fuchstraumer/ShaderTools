15cd6c94ae054f221b03c5f533500e912f28ccdb
fuchstraumer <fuchstraumer@gmail.com>
2025-02-26 09:04:48 -0800

    enum to string mapping, yay!
    Thanks Cursor you actually made this suck a lot less

--- src/common/ShaderToolsErrors.cpp
+++ src/common/ShaderToolsErrors.cpp
@@ -44,12 +44,160 @@ namespace st
     
     const char* ErrorCodeToText(ShaderToolsErrorCode code) noexcept
     {
-        return "null_string";
+        switch (code)
+        {
+            case ShaderToolsErrorCode::Success:
+                return "Success";
+            case ShaderToolsErrorCode::InvalidErrorCode:
+                return "InvalidErrorCode";
+            case ShaderToolsErrorCode::ParserErrorsStart:
+                return "ParserErrorsStart";
+            case ShaderToolsErrorCode::ParserFileNotFound:
+                return "ParserFileNotFound";
+            case ShaderToolsErrorCode::ParserHadNoShaderGroups:
+                return "ParserHadNoShaderGroups";
+            case ShaderToolsErrorCode::ParserHadNoShadersInGroup:
+                return "ParserHadNoShadersInGroup";
+            case ShaderToolsErrorCode::ParserHadInvalidOrEmptyTagsArray:
+                return "ParserHadInvalidOrEmptyTagsArray";
+            case ShaderToolsErrorCode::ParserHadNoResourceGroups:
+                return "ParserHadNoResourceGroups";
+            case ShaderToolsErrorCode::ParserMissingResourceTypeSpecifier:
+                return "ParserMissingResourceTypeSpecifier";
+            case ShaderToolsErrorCode::ParserResourceTypeSpecifierNoVulkanEquivalent:
+                return "ParserResourceTypeSpecifierNoVulkanEquivalent";
+            case ShaderToolsErrorCode::ParserYamlFileHadNoShadersInGroup:
+                return "ParserYamlFileHadNoShadersInGroup";
+            case ShaderToolsErrorCode::ParserYamlFileHadInvalidOrEmptyTagsArray:
+                return "ParserYamlFileHadInvalidOrEmptyTagsArray";
+            case ShaderToolsErrorCode::ParserYamlFileHadInvalidOptimizationLevel:
+                return "ParserYamlFileHadInvalidOptimizationLevel";
+            case ShaderToolsErrorCode::ParserResourceFormatNoVulkanEquivalent:
+                return "ParserResourceFormatNoVulkanEquivalent";
+            case ShaderToolsErrorCode::ParserYamlFileHadInvalidTargetVersion:
+                return "ParserYamlFileHadInvalidTargetVersion";
+            case ShaderToolsErrorCode::GeneratorEmptyIncludePathArray:
+                return "GeneratorEmptyIncludePathArray";
+            case ShaderToolsErrorCode::GeneratorInvalidDescriptorTypeInResourceBlock:
+                return "GeneratorInvalidDescriptorTypeInResourceBlock";
+            case ShaderToolsErrorCode::GeneratorShaderBodyStringNotFound:
+                return "GeneratorShaderBodyStringNotFound";
+            case ShaderToolsErrorCode::GeneratorUnableToFindPreambleFile:
+                return "GeneratorUnableToFindPreambleFile";
+            case ShaderToolsErrorCode::GeneratorUnableToStoreFileContents:
+                return "GeneratorUnableToStoreFileContents";
+            case ShaderToolsErrorCode::GeneratorUnableToAddPreambleToInstanceStorage:
+                return "GeneratorUnableToAddPreambleToInstanceStorage";
+            case ShaderToolsErrorCode::GeneratorUnableToFindMatchingElementOfVertexInterfaceBlockNeededForCompletion:
+                return "GeneratorUnableToFindMatchingElementOfVertexInterfaceBlockNeededForCompletion";
+            case ShaderToolsErrorCode::GeneratorFoundEmptyBodyString:
+                return "GeneratorFoundEmptyBodyString";
+            case ShaderToolsErrorCode::GeneratorUnableToStoreFragmentFileContents:
+                return "GeneratorUnableToStoreFragmentFileContents";
+            case ShaderToolsErrorCode::GeneratorFragmentFileNotFound:
+                return "GeneratorFragmentFileNotFound";
+            case ShaderToolsErrorCode::GeneratorInvalidResourceQualifier:
+                return "GeneratorInvalidResourceQualifier";
+            case ShaderToolsErrorCode::GeneratorUnableToAddInterface:
+                return "GeneratorUnableToAddInterface";
+            case ShaderToolsErrorCode::GeneratorUnableToParseInterfaceBlock:
+                return "GeneratorUnableToParseInterfaceBlock";
+            case ShaderToolsErrorCode::GeneratorUnableToStoreFullSourceString:
+                return "GeneratorUnableToStoreFullSourceString";
+            case ShaderToolsErrorCode::GeneratorUnableToFindLibraryInclude:
+                return "GeneratorUnableToFindLibraryInclude";
+            case ShaderToolsErrorCode::GeneratorUnableToFindLocalInclude:
+                return "GeneratorUnableToFindLocalInclude";
+            case ShaderToolsErrorCode::GeneratorInvalidImageType:
+                return "GeneratorInvalidImageType";
+            case ShaderToolsErrorCode::GeneratorUnableToAddShaderBodyPath:
+                return "GeneratorUnableToAddShaderBodyPath";
+            case ShaderToolsErrorCode::GeneratorUnableToFindEndingOfInterfaceOverride:
+                return "GeneratorUnableToFindEndingOfInterfaceOverride";
+            case ShaderToolsErrorCode::GeneratorNoBodyStringInFileTrackerStorage:
+                return "GeneratorNoBodyStringInFileTrackerStorage";
+            case ShaderToolsErrorCode::CompilerShaderKindNotSupported:
+                return "CompilerShaderKindNotSupported";
+            case ShaderToolsErrorCode::CompilerShaderCompilationFailed:
+                return "CompilerShaderCompilationFailed";
+            case ShaderToolsErrorCode::ReflectionInvalidDescriptorType:
+                return "ReflectionInvalidDescriptorType";
+            case ShaderToolsErrorCode::ReflectionInvalidResource:
+                return "ReflectionInvalidResource";
+            case ShaderToolsErrorCode::ReflectionInvalidBindingIndex:
+                return "ReflectionInvalidBindingIndex";
+            case ShaderToolsErrorCode::ReflectionShaderBinaryNotFound:
+                return "ReflectionShaderBinaryNotFound";
+            case ShaderToolsErrorCode::ReflectionInvalidSpecializationConstantType:
+                return "ReflectionInvalidSpecializationConstantType";
+            case ShaderToolsErrorCode::ReflectionRecompilerError:
+                return "ReflectionRecompilerError";
+            case ShaderToolsErrorCode::ResourceInvalidDescriptorType:
+                return "ResourceInvalidDescriptorType";
+            case ShaderToolsErrorCode::ResourceNotFound:
+                return "ResourceNotFound";
+            case ShaderToolsErrorCode::ShaderPackInvalidDescriptorType:
+                return "ShaderPackInvalidDescriptorType";
+            case ShaderToolsErrorCode::ShaderDoesNotContainGivenHandle:
+                return "ShaderDoesNotContainGivenHandle";
+            case ShaderToolsErrorCode::SubsystemErrorsStart:
+                return "SubsystemErrorsStart";
+            case ShaderToolsErrorCode::FilesystemPathDoesNotExist:
+                return "FilesystemPathDoesNotExist";
+            case ShaderToolsErrorCode::FilesystemPathExistedFileCouldNotBeOpened:
+                return "FilesystemPathExistedFileCouldNotBeOpened";
+            case ShaderToolsErrorCode::FilesystemCouldNotEmplaceIntoInternalStorage:
+                return "FilesystemCouldNotEmplaceIntoInternalStorage";
+            case ShaderToolsErrorCode::FilesystemNoFileDataForGivenHandleFound:
+                return "FilesystemNoFileDataForGivenHandleFound";
+            case ShaderToolsErrorCode::FileTrackerErrorsStart:
+                return "FileTrackerErrorsStart";
+            case ShaderToolsErrorCode::FileTrackerInvalidRequest:
+                return "FileTrackerInvalidRequest";
+            case ShaderToolsErrorCode::FileTrackerReadRequestFailed:
+                return "FileTrackerReadRequestFailed";
+            case ShaderToolsErrorCode::FileTrackerBatchReadRequestFailed:
+                return "FileTrackerBatchReadRequestFailed";
+            case ShaderToolsErrorCode::FileTrackerBatchWriteRequestFailed:
+                return "FileTrackerBatchWriteRequestFailed";
+            case ShaderToolsErrorCode::FileTrackerBatchEraseRequestFailed:
+                return "FileTrackerBatchEraseRequestFailed";
+            case ShaderToolsErrorCode::FileTrackerWriteCouldNotAddPayloadToStorage:
+                return "FileTrackerWriteCouldNotAddPayloadToStorage";
+            case ShaderToolsErrorCode::ShaderStageProcessorErrorsStart:
+                return "ShaderStageProcessorErrorsStart";
+            case ShaderToolsErrorCode::ShaderStageProcessorGivenBodyPathStringDidNotExist:
+                return "ShaderStageProcessorGivenBodyPathStringDidNotExist";
+            default:
+                return "InvalidErrorCode: cannot find error string for this error code";
+        }
     }
 
 	const char* ErrorSourceToTest(ShaderToolsErrorSource source) noexcept
-	{
-        return "null_string";
+	{   
+        switch (source)
+        {
+            case ShaderToolsErrorSource::Parser:
+                return "Parser";
+            case ShaderToolsErrorSource::Generator:
+                return "Generator";
+            case ShaderToolsErrorSource::Compiler:
+                return "Compiler";
+            case ShaderToolsErrorSource::Reflection:
+                return "Reflection";
+            case ShaderToolsErrorSource::Filesystem:
+                return "Filesystem";
+            case ShaderToolsErrorSource::UserInput:
+                return "UserInput";
+            case ShaderToolsErrorSource::ResourceGroup:
+                return "ResourceGroup";
+            case ShaderToolsErrorSource::ShaderPack:
+                return "ShaderPack";
+            case ShaderToolsErrorSource::ShaderStageProcessor:
+                return "ShaderStageProcessor";
+            default:
+                return "InvalidErrorSource: cannot find error source string for this ErrorSource code";
+        }
 	}
 
 }
